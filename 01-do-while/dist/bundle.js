5c52c66fd31abfb87f1c21b5fd68663b9ca19b65
Mariana <mariana@weethub.com>
2023-12-12 10:26:53 -0300

    first commit

---
+++ 01-do-while/dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+var _react = _interopRequireDefault(require("react"));
+
+function interopRequireDefault(obj) { return obj && obj.__esModule ? obj : {"default": obj};}
+
+function App(){
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
---
+++ 01-do-while/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+
+
+// const user ={
+//     name: Mariana,
+// }
+// console.log(user.adress?.street)
