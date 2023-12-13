769b1ee6b18d7fbfc8ac3652f354bda95b8e5719
Mariana <mdxavier@live.com>
2023-12-13 15:17:52 -0300

    add webpacks

--- 01-do-while/dist/bundle.js
+++ 01-do-while/dist/bundle.js
@@ -1,8 +1,8 @@
 "use strict";
 var _react = _interopRequireDefault(require("react"));
 
-function interopRequireDefault(obj) { return obj && obj.__esModule ? obj : {"default": obj};}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : {"default": obj};}
 
 function App(){
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+  return /*#__PURE__*/ _react["default"].createElement("h1", null, "Hello World");
 }
---
+++ 01-do-while/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { App } from './App';
+
+export function Wrapper() {
+    return<h1>Hello World</h1>
+}
